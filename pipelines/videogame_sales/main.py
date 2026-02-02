bac415074db227a26d3dd72d5dcd2cf2bff4d5f0
M4kuro <leonardomapires@gmail.com>
2026-02-02 14:17:06 -0300

    Turned pipeline config drive

--- pipelines/videogame_sales/main.py
+++ pipelines/videogame_sales/main.py
@@ -1,3 +1,4 @@
+import yaml
 from core.extract import extract_csv
 from core.load import load_csv 
 from core.transform import transform_data
@@ -5,19 +6,24 @@ from core.transform import transform_data
 # Business-logic import
 from pipelines.videogame_sales.transform_vgsales import transform_vgsales_data
 
+CONFIG_PATH = "pipelines/videogame_sales/config.yaml"
 
-RAW_PATH = "data/raw/vgsales.csv"
-OUTPUT_PATH = "data/processed/vgsales_clean.csv"
 
 def run():
-    df = extract_csv(RAW_PATH)
+    with open(CONFIG_PATH, "r") as f:
+        config = yaml.safe_load(f)
+
+
+    df = extract_csv(config["raw_path"])
     df = transform_data(df) # Standard transformation
     df = transform_vgsales_data(df) # bussiness-specific transformation
+    
+    load_csv(df, config["output_path"])
 
     print(df.head())
     print(df.dtypes)
 
-    load_csv(df, OUTPUT_PATH)
+    print("Pipeline executed successfully.")
 
 if __name__ == "__main__":
     run()
