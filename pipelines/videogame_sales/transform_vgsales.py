183ee03927ce58f889ff5b0686ee20b2e83fbc41
M4kuro <leonardomapires@gmail.com>
2026-03-16 15:34:59 -0300

    Edited bussiness logic to upgrade fact table, so deeper insight can be made while building dashboards; Add Platforms and Publisher columns to dataset.

--- pipelines/videogame_sales/transform_vgsales.py
+++ pipelines/videogame_sales/transform_vgsales.py
@@ -11,25 +11,28 @@ def transform_vgsales_data(df: pd.DataFrame) -> pd.DataFrame:
     
     # Keep only required columns
     required_columns = [
-        "year",
-        "genre",
-        "na_sales",
-        "eu_sales",
-        "jp_sales",
-        "other_sales",
-        
-    ]
+    "year",
+    "genre",
+    "platform",
+    "publisher",
+    "na_sales",
+    "eu_sales",
+    "jp_sales",
+    "other_sales"
+]
     
     df = df[required_columns]
     
-    df=df.rename(  
-        columns={
-            "na_sales": "NA",
-            "eu_sales": "EU",
-            "jp_sales": "JP",
-            "other_sales": "Other",
-        }
-    )
+    df = df.rename(
+    columns={
+        "na_sales": "NA",
+        "eu_sales": "EU",
+        "jp_sales": "JP",
+        "other_sales": "Other",
+        "publisher": "pub",
+        "platform": "plats"
+    }
+)
     
     # Drop rows with missing year
     df = df.dropna(subset=["year"]) 
@@ -41,22 +44,25 @@ def transform_vgsales_data(df: pd.DataFrame) -> pd.DataFrame:
     df["year"] = df["year"].astype(int)
     
     # Convert wide → long format
+    # This is necessary to make it easier to analyze sales by region and genre over time.
     df_long = df.melt(  
-        id_vars=["year", "genre"],
+        id_vars=["year", "genre", "plats", "pub"],
         value_vars=["NA", "EU", "JP", "Other"],
         var_name="region",
-        value_name="sales",
+        value_name="sales",        
     )
     
     df_long = df_long[df_long["sales"] > 0] 
 
-    # Aggregate
+    # Aggregate sales by year, genre, and region and publisher
     df_long = (
         df_long  
-        .groupby(["year","genre", "region"], as_index=False)
-        .agg(total_sales=("sales", "sum"))
+        .groupby(["year","genre","plats","region","pub"], as_index=False)
+        .agg(sales=("sales", "sum")).round(2) # Round to 2 decimals; Prevent those ginormous weird numbers
         .sort_values("year")
     )
     
-
+    
+    
+    
     return df_long
